5a0b80e65ae768150913ede495dcbd0b84cdb821
martian2049 <ginx.qx@gmail.com>
2017-04-23 20:54:37 +0800

    embeded, saved base1000

--- crawl/1.py
+++ crawl/1.py
@@ -27,21 +27,24 @@ import numpy as np
 from web.crawl import get_defcrawl
 from chars import get1kdict
 dict1k,dict1kem,base1000 = get1kdict()
-for i in range(945,len(base1000)):
-    base=base1000[i]
-    print(base,i)
-    defcrawl=get_defcrawl(base)
-    f=open('data-mini/defcrawl/'+str(i)+base+'.txt','w')
-    f.write(defcrawl)
-    f.close()
+# for i in range(len(base1000)):
+#     base=base1000[i]
+#     print(base,i)
+#     # defcrawl=get_defcrawl(base)
+#     # f=open('data-mini/defcrawl/base1000/'+str(i)+base+'.txt','w')
+#     # f.write(defcrawl)
+#     # f.close()
+#     f=open('data-mini/defcrawl/base1000/'+str(i)+base+'.txt','rb')
+#     defcrawl=[l.decode().strip() for l in f.readlines()][0]
+#     # print(defcrawl)
 #     invalid_chars=''.join([char for i,char in enumerate(defcrawl) if char not in dict1kem and char!=base])
 #     valid_chars=''.join([char for i,char in enumerate(defcrawl) if char in dict1kem and char!=base])
 #     candidate=[dict1k[char] for i,char in enumerate(valid_chars)]
 #     dict1kem[base]+=np.array(candidate).sum(axis=0)
 #     # print("\ndict1kem["+base+"][0:10]:==> ", dict1kem[base][0:10],'\n')
 
-# # save all embeddings here:
+# save all embeddings here:
 # np.save('save/dict1kem.npy',dict1kem)
-# newdict1kem=np.load('save/dict1kem.npy').item()
-# print(newdict1kem['我'])
+newdict1kem=np.load('save/dict1kem.npy').item()
+print(newdict1kem['头'])
 
